804c3bdf364da0ed229d1e1f4cb699ac8adc84df
Teru0220 <73335795+Teru0220@users.noreply.github.com>
2026-09-04 02:35:51 +0900

    Q35でログインまで確認。不要箇所を削る予定

--- main.tf
+++ main.tf
@@ -5,27 +5,54 @@ resource "libvirt_domain" "worker_node" {
   memory = 8192
   memory_unit = "MiB"
   vcpu   = 2
-  type        = "kvm"
+  type   = "kvm"
 
   os = {
     type         = "hvm"
     type_arch    = "x86_64"
-    type_machine = "pc"
-    boot_devices = [
-      { dev = "hd" }
+    type_machine = "q35" # Q35 を使用
+    boot_devices = [{ dev = "hd" }]
+  }
+
+  cpu = {
+    mode = "host-passthrough"
+  }
+
+  metadata = {
+    xml ="<libosinfo:libosinfo xmlns:libosinfo='http://libosinfo.org/xmlns/libvirt/domain/1.0'><libosinfo:os id='http://ubuntu.com/ubuntu/24.04'/></libosinfo:libosinfo>"
+  }
+
+  features = {
+    acpi = true
+    apic = {}
+    vm_port = { 
+      state = "off"
+    }
+  }
+
+  # Control Plane と同一のタイマー設定（HPET 無効化）
+  clock = {
+    offset = "utc"
+    timer = [
+      { name = "rtc",  tick_policy = "catchup" },
+      { name = "pit",  tick_policy = "delay" },
+      { name = "hpet", present = "no" }
     ]
   }
 
   devices = {
     disks = [
+      # メインの VirtIO ディスク
       {
         source = {
-          file = {
-            file = "/var/lib/libvirt/images/worker-${format("%02d", count.index + 1)}.qcow2"
+          volume = {
+            pool   = libvirt_volume.worker_disk[count.index].pool
+            volume = libvirt_volume.worker_disk[count.index].name
           }
         }
         driver = {
-          type = "qcow2"
+          type    = "qcow2"
+          discard = "unmap"
         }
         target = {
           dev = "vda"
@@ -33,34 +60,44 @@ resource "libvirt_domain" "worker_node" {
         }
       }
     ]
+
     interfaces = [
       {
-        model = {
-          type = "virtio"
-        }
-        source = {
-          network = {
-            network = "default"
-          }
-        }
+        model  = { type = "virtio" }
+        source = { network = { network = "default" } }
       }
     ]
+
     consoles = [
+      { target = { type = "serial", port = "0" } }
+    ]
+
+    graphics = [
+      { spice = { auto_port = true, listeners = [{ address = {} }] } }
+    ]
+
+    videos = [
+      { model = { type = "virtio" } }
+    ]
+
+    channels = [
       {
-        target = {
-          type = "serial"
-          port = "0"
-        }
+        source = { unix = {} }
+        target = { virt_io = { name = "org.qemu.guest_agent.0" } }
+      },
+      {
+        source = { spice_vmc = true }
+        target = { virt_io = { name = "com.redhat.spice.0" } }
       }
     ]
-    graphics = [
+
+    # エントロピー（乱数）不足による起動遅延を防ぐ VirtIO-RNG
+    rngs = [
       {
-        spice = {
-          autoport = true
+        backend = {
+          random = "/dev/urandom"
         }
-        listen = {
-          type = "address"
-        }
+        model = "virtio"
       }
     ]
   }
@@ -71,7 +108,7 @@ resource "libvirt_volume" "worker_disk" {
   count    = var.worker_count
   name     = format("worker-%02d.qcow2", count.index + 1)
   pool     = "default"
-  capacity = 42949672960 # 40GB (バイト指定)
+  capacity = 42949672960 # 40GB
 
   target = {
     format = {
